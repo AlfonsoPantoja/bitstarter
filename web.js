f768f4834915205c00e637956c95a6fc03358545
Alfonso Pantoja <alfonso.pantoja@gmail.com>
2013-07-09 16:40:10 +0000

    Read content from file

--- web.js
+++ web.js
@@ -1,9 +1,25 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var getContentFromFile = function (filename) {
+
+    var bufferFile = fs.readFileSync(filename);
+    if (bufferFile) {
+	return bufferFile.toString();
+    }
+    else {
+
+	return "content file index.html not found";
+    }
+};
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+
+  response.send(getContentFromFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
